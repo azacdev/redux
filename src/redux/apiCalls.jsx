3a3c4fdaa177b3e8228c5cc68e3ab35174e05990
azacdev <azacdev@gmail.com>
2023-07-04 23:09:14 +0100

    update

--- src/redux/apiCalls.jsx
+++ src/redux/apiCalls.jsx
@@ -4,7 +4,10 @@ import axios from "axios";
 const updateUser = async (user, dispatch) => {
   dispatch(updateStart());
   try {
-    const res = await axios.get("https://localhost:8800/api/users/123/update");
+    const res = await axios.get(
+      "https://localhost:8800/api/users/123/update",
+      user
+    );
     dispatch(updateSuccess(res.data));
   } catch (error) {
     dispatch(updateError());
